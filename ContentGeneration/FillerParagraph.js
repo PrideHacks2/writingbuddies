e805714942fad4f6c82f91f189e0e28b2ca83e6d
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-06-25 19:29:22 +0800

    Add functionality for the filler paragraph generation

--- ContentGeneration/FillerParagraph.js
+++ ContentGeneration/FillerParagraph.js
@@ -1,13 +1,44 @@
-
-
 const outputText = document.getElementById("output-text");
-
-
+const loadingSpinner = document.getElementById("loading-spinner");
 
 function copyOutput() {
-    navigator.clipboard.writeText(outputText.innerText);
+  navigator.clipboard.writeText(outputText.innerText);
 }
 
-function generateParagraph() {
-    
+async function generateParagraph() {
+  // Get the text from the textarea
+  const textInput = document.getElementById("writing-input").value;
+
+  loadingSpinner.style.display = "block";
+
+  const data = {
+    prompt: textInput,
+    max_tokens: 100,
+    temperature: 0.8,
+    k: 0,
+    p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop_sequences: ["--"],
+    return_likelihoods: "NONE",
+  };
+
+  fetch("https://api.cohere.ai/large/generate", {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "BEARER Bz9P1eAbsWbGZn9F9Q55xEkpp28Kwjz6868HOMAX",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      loadingSpinner.style.display = "none";
+      console.log("Success:", data);
+      outputText.innerText = data.text;
+    })
+    .catch((error) => {
+      loadingSpinner.style.display = "none";
+      console.error("Error:", error);
+    });
 }
