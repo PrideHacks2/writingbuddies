71f15405fa71a3a54a13b0a16f91e2216926c7bb
franceneee <57038559+franceneee@users.noreply.github.com>
2022-06-25 20:39:44 +0800

    Add monster and hp bar

--- TypingGame/TypingGame.js
+++ TypingGame/TypingGame.js
@@ -4,10 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const gameContainer = document.getElementById("gameContainer");
     const textInput = document.getElementById("textInput");
     const showWordCount = document.getElementById("showWordCount");
+    const monster = document.getElementById("monster");
+    const hp = document.getElementById("hp");
+    let monsterIsDead = false;
 
-    //localStorage.clear();
+    localStorage.setItem("goal", 123);
+
     if (localStorage.getItem("goal") !== null) {
         modal.style.display = "none";
+        monster.style.display = "flex";
     }
 
     form.addEventListener("submit", function (e) {
@@ -23,14 +28,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     textInput.addEventListener("input", function(){
-        var v = count_word( this.value );
+        let input = countWord( this.value );
         showWordCount.innerHTML = (
-            "<br>Words: "+ v.words
-        );
+            "<br>Words: "+ input.words
+        ); 
+
+        hp.style.width = 100 - Math.floor(input.words*100/localStorage.getItem("goal")) + "%";
+        hp.innerHTML = 100 - Math.floor(input.words/localStorage.getItem("goal")*100) + "%";
+        if(hp.innerHTML <= 0) hp.innerHTML = 0 + "%";
       }, false);
 
-    function count_word( val ){
+    function countWord( val ){
         var wom = val.match(/\S+/g);
+        if (wom.length >= localStorage.getItem("goal")) monsterIsDead = true;
+
+        if (monsterIsDead === true) {
+            monster.style.display = "none";
+        }
+
         return {
             words : wom ? wom.length : 0
         };
