bce821dce569b846dbd5ae81f642e1b1e6462639
franceneee <57038559+franceneee@users.noreply.github.com>
2022-06-25 19:55:36 +0800

    Add word counter

--- TypingGame/TypingGame.js
+++ TypingGame/TypingGame.js
@@ -1,1 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("modal");
+    const form = document.getElementById("form");
+    const gameContainer = document.getElementById("gameContainer");
+    const textInput = document.getElementById("textInput");
+    const showWordCount = document.getElementById("showWordCount");
+
+    //localStorage.clear();
+    if (localStorage.getItem("goal") !== null) {
+        modal.style.display = "none";
+    }
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        console.log(form.goal.value);
+        localStorage.setItem("goal", form.goal.value);
+    }); 
+
+    if (localStorage.getItem("goal") !== null) {
+        gameContainer.style.display = "flex";
+    } else {
+        gameContainer.style.display = "none";
+    }
+
+    textInput.addEventListener("input", function(){
+        var v = count_word( this.value );
+        showWordCount.innerHTML = (
+            "<br>Words: "+ v.words
+        );
+      }, false);
+
+    function count_word( val ){
+        var wom = val.match(/\S+/g);
+        return {
+            words : wom ? wom.length : 0
+        };
+    }
+});
